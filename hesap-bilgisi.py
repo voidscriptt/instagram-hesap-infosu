aac664d91d8cf4793c6bc46fac24edb3e9f50a44
Arjin <149720005+voidscriptt@users.noreply.github.com>
2023-11-02 23:30:23 +0300

    Add files via upload

---
+++ hesap-bilgisi.py
@@ -0,0 +1,20 @@
+import instaloader
+
+username = input("Instagram kullanıcı adını girin: ")
+
+L = instaloader.Instaloader()
+
+try:
+    L.download_profile(username, profile_pic_only=True)
+    
+
+    profile = instaloader.Profile.from_username(L.context, username)
+
+    print(f"Kullanıcı Adı: {profile.username}")
+    print(f"Gönderi Sayısı: {profile.mediacount}")
+    print(f"Takipçi Sayısı: {profile.followers}")
+    print(f"Takip Ettiği Kişi Sayısı: {profile.followees}")
+except instaloader.exceptions.ProfileNotExistsException:
+    print(f"'{username}' kullanıcısı bulunamadı.")
+except Exception as e:
+    print(f"Hata oluştu: {e}")
